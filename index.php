54eed42ffaa195003c66c64d41073cd12ecac629
TAYAN LIMA CORDEIRO <TAYANCORDEIRO@natura.net>
2019-06-24 16:24:54 -0300

    Structure to project details

--- index.php
+++ index.php
@@ -10,37 +10,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!-- translate file -->
 <html>
 <head>
-	<title><?php echo e("im") ?></title>
-	<link rel="icon"
-	type="image/png"
-	href="images/favicon.JPG">
-	<!--fonts-->
-	<link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
-	<!--//fonts-->
-	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-	<!-- for-mobile-apps -->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="keywords" content="Portfolio, Tayan, Lima, Cordeiro" />
-	<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-	<!-- //for-mobile-apps -->
-	<!-- js -->
-	<script type="text/javascript" src="js/jquery.min.js"></script>
-	<!-- js -->
-	<!-- start-smooth-scrolling -->
-	<script type="text/javascript" src="js/move-top.js"></script>
-	<script type="text/javascript" src="js/easing.js"></script>
-	<script type="text/javascript">
-		jQuery(document).ready(function($) {
-			$(".scroll").click(function(event){
-				event.preventDefault();
-				$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
-			});
-		});
-	</script>
-	<!-- start-smooth-scrolling -->
+	<?php include("head.php"); ?>
 
 </head>
 <body>
