59056ddd3700c5cc4250fa2038436a46affaeadd
TAYAN LIMA CORDEIRO <TAYANCORDEIRO@natura.net>
2019-06-25 15:10:19 -0300

    Structure project details

--- p.php
+++ p.php
@@ -31,10 +31,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 	$ptitle = "projects-title-".$pid; 
 	$pdetail = "projects-detail-".$pid; 
 
-
-	//todo Falta dinamizar galeria
-
-	 ?>
+	?>
 
 	<!-- banner -->
 	<div class="banner" id="home">
@@ -42,14 +39,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 
 			<div class="top-header row">
 				<script src="js/classie.js"></script>
-				<!--top-nav---->
-				<div class="top-nav">
-					<p class="change-lang"> <?php echo e("change-lang") ?> </p>	
-					<a href="?lang=pt" class="right_bt" title="<?php echo e("lang-name-1") ?>"><img style="width: 2em; height: 2em;" src="images/lang_pt.png" alt=""/></a>
-					<a href="?lang=en" class="right_bt" title="<?php echo e("lang-name-2") ?>"><img style="width: 2em; height: 2em;" src="images/lang_en.png" alt=""/></a>
-					<a href="?lang=es" class="right_bt" title="<?php echo e("lang-name-3") ?>"><img style="width: 2em; height: 2em;" src="images/lang_es.png" alt=""/></a>
-				</div>
-
+				
 				<div class="gallery-section text-center" id="portfolio" style="background: none;">
 					<div class="container">
 						<h3><?php echo e($pname) ?></h3>
@@ -61,27 +51,6 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 							<div class="top-gallery">
 
 								<?php 
-
-								/*$filelist = array();
-								$path = "C:\Program Files (x86)\Ampps\www\portfolio\images\projects\p".$pid;
-								if ($handle = opendir($path)) {
-									while ($entry = readdir($handle)) {
-										echo $entry;
-										$true = is_file($entry);
-										echo $true;
-										if (is_file($entry)) {
-											$filelist[] = $entry;
-										}
-									}
-									closedir($handle);
-								}
-
-								echo($filelist[0]);
-								echo($filelist[1]);
-								echo($filelist[2]);*/
-
-
-
 
 								$entries = scandir(".\images\projects\p".$pid);
 								$filelist = array();
@@ -100,30 +69,10 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 										
 										echo "<div class='col-md-4 gallery-grid gallery1'><img src='images/projects/p".$pid."/".$value."' class='img-responsive' alt='/''></div>";
 									}
-
-/*									echo $value;
-									echo " - ";*/
-										
+									
 								}	 
 
 								?>
-								
-
-								<!-- <div class="col-md-4 gallery-grid gallery1">
-									<img src="images/p5.jpg" class="img-responsive" alt="/">
-								</div>
-								<div class="col-md-4 gallery-grid gallery1">
-									<img src="images/p5.jpg" class="img-responsive" alt="/">
-								</div>
-								<div class="col-md-4 gallery-grid gallery1">
-									<img src="images/p5.jpg" class="img-responsive" alt="/">
-								</div>
-								<div class="col-md-4 gallery-grid gallery1">
-									<img src="images/p5.jpg" class="img-responsive" alt="/">
-								</div>
-								<div class="col-md-4 gallery-grid gallery1">
-									<img src="images/p5.jpg" class="img-responsive" alt="/">
-								</div> -->
 
 								<div class="clearfix"> </div>
 							</div>
